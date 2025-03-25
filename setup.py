e4ca704837de45dc2874505314a7628ab8f9da22
Diego <dgarciacosio@gmail.com>
2025-03-25 13:46:21 +0100

    Versión incrementada

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="albutils",  # Nombre del paquete
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     install_requires=[],  # Dependencias (puedes agregar más si las necesitas)
     author="Diego García",
